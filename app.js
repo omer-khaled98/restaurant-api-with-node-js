825dce6b54fc48656654f061d3d7faf1f83cfff9
Omer kh <omerkhaled123789456@gmail.com>
2024-10-15 19:48:51 +0300

    update packages

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:4200",
   "http://localhost:5173",
+"https://restaurant-website-with-react-eight.vercel.app",
 ];
 
 // Middleware
